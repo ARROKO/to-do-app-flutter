690d64217ba55c7b4310358fb1ff138c4f6c9cf0
ARROKO <kemgang605@gmail.com>
2023-12-31 14:57:57 +0100

    first commit

---
+++ lib/pages/notes_pages.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotePages extends StatefulWidget {
+  const NotePages({super.key});
+
+  @override
+  State<NotePages> createState() => _NotePagesState();
+}
+
+class _NotePagesState extends State<NotePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
